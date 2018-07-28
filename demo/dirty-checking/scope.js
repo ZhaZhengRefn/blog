46a74d62d8d10628c89dd6ad74ed520c4d16da4c
ZhaZhengRefn <512557403@qq.com>
2018-07-28 16:57:56 +0800

    更新脏检查简单实现

---
+++ demo/dirty-checking/scope.js
@@ -0,0 +1,85 @@
+class Scope {
+  constructor() {
+    this.$$watcher = []
+    /**
+     * watcher数据结构
+     * {
+     *  name: name,//变量名
+     *  oldVal: '',//旧值
+     *  newVal: exp,//返回新值的函数
+     *  listener: listener || function() {},//监听回调
+     * }
+     */
+  }
+
+  //添加watcher
+  $watch(name, exp, listener = () => {}) {
+    const oldVal = exp()
+    this.$$watcher.push({
+      name,
+      oldVal,
+      newVal: exp,
+      listener
+    })
+  }
+
+  //脏检查关键之处，遍历watcher，比对旧值与新值。再触发listener
+  $digest() {
+    let dirty = true
+    while (dirty) {
+      const bindElements = [...document.querySelectorAll("[ng-bind]")]
+      dirty = false
+      this.$$watcher.forEach(cur => {
+        const newVal = cur.newVal()
+        if (cur.oldVal !== newVal) {
+          dirty = true
+          cur.oldVal = newVal
+          cur.listener(cur.oldVal, newVal)
+          //数据变动时操作dom
+          bindElements.forEach(el => {
+            const dataName = el.getAttribute("ng-bind")
+            if (dataName === cur.name) {
+              if (el.tagName === 'INPUT') {
+                el.value = this[dataName]
+              } else {
+                el.innerHTML = this[dataName]
+              }
+            }
+          })
+        }
+      })
+    }
+  }
+
+  $init() {
+    const _this = this
+
+    //监听所有数据，添加watcher
+    for (const key in this) {
+      if (this.hasOwnProperty(key)) {
+        const cur = this[key];
+        if (key !== '$watch' && key !== '$digest' && key !== '$$watcher' && typeof cur !== 'function') {
+          this.$watch(key, function (key) {
+            return function () {
+              return _this[key]
+            }
+          }(key))
+        }
+      }
+    }    
+
+    //解析所有ng-click指令
+    const clickElements = document.querySelectorAll("[ng-click]")
+    clickElements.forEach(el => {
+      el.onclick = function() {
+        const methodName = el.getAttribute('ng-click')
+        if (typeof _this[methodName] === 'function') {
+          _this[methodName].call(_this)
+          _this.$digest()
+        }
+      }
+    })
+  }
+}
+
+window.Scope = Scope
